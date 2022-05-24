3ea88cb10696a3ebcf658bc3ee8042d44929aa3f
MartiniAlini <105885407+MartiniAlini@users.noreply.github.com>
2022-05-24 12:13:40 -0300

    Add files via upload

---
+++ litosfera/jsindex.js
@@ -0,0 +1,10 @@
+
+
+
+
+var botaoMenu = document.querySelector('#e')
+	var menu = document.querySelector('menuSuspenso')
+
+	botaoMenu.addEventListener('click', () => {
+	menu.classList.toggle('menuAtivo')
+})
